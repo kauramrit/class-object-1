0942ac7b59c4e0955f477a34c940a04039f14321
kauramrit <42332568+kauramrit@users.noreply.github.com>
2018-08-25 01:50:30 +0530

    Update class1.py

--- class1.py
+++ class1.py
@@ -2,12 +2,17 @@
 
 #Q.1- Create a user defined dictionary and get keys corresponding to the value using for loop.
 
-a=[1,2,3]
-b=['a','b','c']
-c={}
-for i in range(len(a)):
-    c[a[i]]=b[i]
-print(c)
+dict={}
+n=int(input("Enter number of items: "))
+for x in range(n):
+    key=input("Enter key ")
+    value=input("Enter value ")
+    dict[key]=value
+print(dict)
+val=input("Enter value to find")
+for y,z in dict.items():
+    if(z==val):
+        print("Key of value",val,"is",y)
 
 #Q.2- Create a dictionary and store student names and create nested dictionary to store the subject wise marks of every student.
 #Print the marks of a given student from that dictionary for every subject. 
